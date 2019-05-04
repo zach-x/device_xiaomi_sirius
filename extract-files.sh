2dbd2d1fb3073687fa6f0a71d41f3e627f95545e
guoxiao1213 <598464949@qq.com>
2019-05-04 20:40:20 +0800

    Init to MI 8SE

--- extract-files.sh
+++ extract-files.sh
@@ -5,10 +5,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-set -e
-
-export DEVICE=dipper
-export DEVICE_COMMON=sdm845-common
+export DEVICE=sirius
+export DEVICE_COMMON=sdm710-common
 export VENDOR=xiaomi
 
 export DEVICE_BRINGUP_YEAR=2018
