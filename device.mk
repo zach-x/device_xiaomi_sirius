a257ab9aa7ae6b25b75e9d011952d179b565819c
ZachX <51441501+zach-x@users.noreply.github.com>
2019-06-27 19:59:02 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -18,7 +18,8 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 # Boot animation
-TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SCREEN_HEIGHT := 2244
+TARGET_SCREEN_WIDTH := 1080
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
